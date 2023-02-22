6f62934f657c3a6a914db6fb1f69629e3d00284b
Ritu Bansal <Ritu.Bansal@WinWire.com>
2023-02-22 20:21:40 +0530

    Assignment 2 of Layout Designs

---
+++ Xamarin_Assignment/Xamarin_Assignment/Layout_2.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin_Assignment
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Layout_2 : ContentPage
+    {
+        public Layout_2()
+        {
+            InitializeComponent();
+         
+        }
+    }
+}
---
+++ Xamarin_Assignment/Xamarin_Assignment/Layout_3.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin_Assignment
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Layout_3 : ContentPage
+    {
+        public Layout_3()
+        {
+            InitializeComponent();
+        }
+    }
+}
